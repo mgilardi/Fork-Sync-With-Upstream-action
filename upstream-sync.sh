bffd746b9670c80a74f5e8d08d21e426892313e7
Michael Gilardi <mgilardi@asu.edu>
2021-03-30 12:15:01 -0700

    Update upstream-sync.sh

--- upstream-sync.sh
+++ upstream-sync.sh
@@ -54,92 +54,49 @@ reset_git() {
 ### --------------- ###
 ### script below    ###
 
-# fail if upstream_repository is not set in workflow
-# if [ -z "${INPUT_UPSTREAM_REPOSITORY}" ]; then
-#     echo 'Workflow missing input value for "upstream_repository"' 1>&2
-#     echo '      example: "upstream_repository: aormsby/fork-sync-with-upstream-action"' 1>&2
-#     exit 1
-# else
-#     UPSTREAM_REPO="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${INPUT_UPSTREAM_REPOSITORY}.git"
-# fi
-
 # set user credentials in git config
 config_git
+git config --global push.default current
 
-# ensure target_branch is checked out
-# if [ $(git branch --show-current) != "${INPUT_TARGET_BRANCH}" ]; then
-#     git checkout ${INPUT_GIT_CHECKOUT_ARGS} "${INPUT_TARGET_BRANCH}"
-#     echo 'Target branch ' ${INPUT_TARGET_BRANCH} ' checked out' 1>&1
-# fi
-
-# set upstream to upstream_repository
-# git remote add upstream "${UPSTREAM_REPO}"
-
-# check remotes in case of error
-# git remote -v
-
-# check latest commit hashes for a match, exit if nothing to sync
-# git fetch ${INPUT_GIT_FETCH_ARGS} upstream "${INPUT_UPSTREAM_BRANCH}"
-# LOCAL_COMMIT_HASH=$(git rev-parse "${INPUT_TARGET_BRANCH}")
-# UPSTREAM_COMMIT_HASH=$(git rev-parse upstream/"${INPUT_UPSTREAM_BRANCH}")
-
-# if [ "${LOCAL_COMMIT_HASH}" = "${UPSTREAM_COMMIT_HASH}" ]; then
-#     echo "::set-output name=has_new_commits::false"
-#     echo 'No new commits to sync, exiting' 1>&1
-#     reset_git
-#     exit 0
-# fi
-
-# echo "::set-output name=has_new_commits::true"
-# # display commits since last sync
-# echo 'New commits being synced:' 1>&1
-# git log upstream/"${INPUT_UPSTREAM_BRANCH}" "${LOCAL_COMMIT_HASH}"..HEAD ${INPUT_GIT_LOG_FORMAT_ARGS}
-
-# # sync from upstream to target_branch
-# echo 'Syncing...' 1>&1
-# # pull_args examples: "--ff-only", "--tags"
-# git pull --no-edit ${INPUT_GIT_PULL_ARGS} upstream "${INPUT_UPSTREAM_BRANCH}"
-# echo 'Sync successful' 1>&1
-
-# # push to origin target_branch
-# echo 'Pushing to target branch...' 1>&1
-# git push ${INPUT_GIT_PUSH_ARGS} origin "${INPUT_TARGET_BRANCH}"
-# echo 'Push successful' 1>&1
-
-echo 'Fetching origin...'
 git fetch --prune --unshallow
 
-echo 'Tracking all relevant branches...'
-for remote in $(git branch -r); do
-    if [ "$remote" != 'origin/main' ]; then
-        git branch --track ${remote#origin/} $remote
-    fi
-done
+git pull --no-edit origin main
+git push
 
-for branch in $(git for-each-ref --format='%(refname:short)' --sort='*refname:short' refs/heads/); do
-    echo $branch
+# echo 'Fetching origin...'
+# git fetch --prune --unshallow
 
-    case "$branch" in
-        *\/*) 
-            echo "Branch has a forward slash in it, ignoring..."
-            ;;
-        *)
-            echo 'Attempting to pull non-destructive changes from main to '
-            echo -ne "$branch"
-            git checkout $branch
-            git pull origin main
-            var2=$?
-            if [ "var2" = "0" ]; then
-                git push origin $branch
-            else
-                git merge --abort
-            fi
-            ;;
-    esac
-done
+# echo 'Tracking all relevant branches...'
+# for remote in $(git branch -r); do
+#     if [ "$remote" != 'origin/main' ]; then
+#         git branch --track ${remote#origin/} $remote
+#     fi
+# done
 
-echo 'Checking out main before finishing...'
-git checkout -f main
+# for branch in $(git for-each-ref --format='%(refname:short)' --sort='*refname:short' refs/heads/); do
+#     echo $branch
+
+#     case "$branch" in
+#         *\/*) 
+#             echo "Branch has a forward slash in it, ignoring..."
+#             ;;
+#         *)
+#             echo 'Attempting to pull non-destructive changes from main to '
+#             echo -ne "$branch"
+#             git checkout $branch
+#             git pull --no-edit origin main
+#             echo $?
+#             if [ $? -eq 0 ]; then
+#                 git push origin $branch
+#             else
+#                 git merge --abort
+#             fi
+#             ;;
+#     esac
+# done
+
+# echo 'Checking out main before finishing...'
+# git checkout -f main
 
 # reset user credentials for future actions
 reset_git
